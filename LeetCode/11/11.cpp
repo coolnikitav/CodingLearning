36ffb2a1d637179d9b38ff36f0c725a992380f80
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-07-29 17:50:40 -0400

    Create 11.cpp

---
+++ LeetCode/11/11.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+
+        /*
+         * Find area between beginning and end. Then move away from the 
+         * smaller number and check the new area.
+         */
+        for (int front = 0, back = height.size()-1; front != back;) {
+            int tempArea = min(height[front], height[back]) * (back - front);
+            if (tempArea > area) {
+                area = tempArea;
+            }
+            if (height[front] <= height[back]) {
+                front++;
+            } else {
+                back--;
+            }
+        }
+        return area;
+    }
+};
