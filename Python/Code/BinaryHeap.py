3f0611580acddd0fa5f92d45504f194d2d26afb0
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-08-15 08:47:23 -0400

    Update BinaryHeap.py

--- Python/Code/BinaryHeap.py
+++ Python/Code/BinaryHeap.py
@@ -28,3 +28,95 @@ def levelOrderTraversal(rootNode):
             print(rootNode.customList[i])
 
 levelOrderTraversal(newBinaryHeap)
+
+def heapifyTreeInsert(rootNode, index, heapType):
+    parentIndex = int(index/2)
+    if index <= 1:
+        return
+    if heapType == "Min":
+        if rootNode.customList[index] < rootNode.customList[parentIndex]:
+            temp = rootNode.customList[index]
+            rootNode.customList[index] = rootNode.customList[parentIndex]
+            rootNode.customList[parentIndex] = temp
+        heapifyTreeInsert(rootNode, parentIndex, heapType)
+    elif heapType == "Max":
+        if rootNode.customList[index] > rootNode.customList[parentIndex]:
+           temp = rootNode.customList[index]
+           rootNode.customList[index] = rootNode.customList[parentIndex]
+           rootNode.customList[parentIndex] = temp 
+        heapifyTreeInsert(rootNode, parentIndex, heapType)     
+
+def insertNode(rootNode, nodeValue, heapType):
+    if rootNode.heapSize + 1 == rootNode.maxSize:
+        return "The Binary Heap is full"
+    rootNode.customList[rootNode.heapSize + 1] = nodeValue
+    rootNode.heapSize += 1
+    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)   
+    return "The value has been successfully inserted"
+
+insertNode(newHeap, 4, "Max")
+insertNode(newHeap, 5, "Max")
+insertNode(newHeap, 2, "Max")
+insertNode(newHeap, 1, "Max")
+levelOrderTraversal(newHeap)
+
+def heapifyTreeInsert(rootNode, index, heapType):
+    leftIndex = index * 2
+    rightIndex = index * 2 + 1
+    swapChild = 0
+
+    if rootNode.heapSize < leftIndex:
+        return
+    elif rootNode.heapSize == leftIndex:
+        if heapType == "Min":
+            if rootNode.customList[index] > rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+        else:
+            if rootNode.customList[index] < rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+    else:
+        if heapType == "Min":
+            if rootNode.customList[leftIndex] < rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] > rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+        else:
+            if rootNode.customList[leftIndex] > rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] < rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+        heapifyTreeInsert(rootNode, swapChild, heapType)
+
+def extractNode(rootNode, heapType):
+    if rootNode.heapSize == 0:
+        return
+    else: 
+        extractedNode = rootNode.customList[1]
+        rootNode.customList[1] = rootNode.customList[rootNode.heapSize]
+        rootNode.customList[rootNode.heapSize] = None
+        rootNode.heapSize -= 1
+        heapifyTreeInsert(rootNode, 1, heapType)
+        return extractedNode
+
+print(extractNode(newHeap, "Max"))
+levelOrderTraversal(newHeap)
+
+def deleteEntireBH(rootNode):
+    rootNode.customList = None
+
+deleteEntireBH(newHeap)
+levelOrderTraversal(newHeap)
