96434701103ea561a172ec436492bd5b2585fc34
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-09-14 14:13:15 -0400

    Update convert_string.py

--- Python/Code/convert_string.py
+++ Python/Code/convert_string.py
@@ -33,3 +33,25 @@ def convert_string(s1, s2, index1, index2, temp_dict):
     return temp_dict[dict_key]
 
 # Bottom Up
+def findMinOperationBU(s1, s2, tempDict):
+    for i1 in range(len(s1)+1):
+        dictKey = str(i1)+'0'
+        tempDict[dictKey] = i1
+    for i2 in range(len(s2)+1):
+        dictKey = '0'+str(i2)
+        tempDict[dictKey] = i2
+    
+    for i1 in range(1,len(s1)+1):
+        for i2 in range(1,len(s2)+1):
+            if s1[i1-1] == s2[i2-1]:
+                dictKey = str(i1)+str(i2)
+                dictKey1 = str(i1-1)+str(i2-1)
+                tempDict[dictKey] = tempDict[dictKey1]
+            else:
+                dictKey = str(i1)+str(i2)
+                dictKeyD = str(i1-1)+str(i2)
+                dictKeyI = str(i1)+str(i2-1)
+                dictKeyR = str(i1-1)+str(i2-1)
+                tempDict[dictKey] = 1 + min(tempDict[dictKeyD], min(tempDict[dictKeyI],tempDict[dictKeyR]))
+    dictKey = str(len(s1))+str(len(s2))
+    return tempDict[dictKey]
