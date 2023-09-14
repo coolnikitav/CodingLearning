448669d08c23fb8e2d7bf042f151d15a62f6d0ed
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-09-14 14:10:33 -0400

    Update convert_string.py

--- Python/Code/convert_string.py
+++ Python/Code/convert_string.py
@@ -1,3 +1,4 @@
+# Divide and Conquer
 def find_min_operation(s1, s2, index1, index2):
   if index1 == len(s1):
     return len(s2) - index2
@@ -12,3 +13,23 @@ def find_min_operation(s1, s2, index1, index2):
     return min(delete_op, insert_op, replace_op)
 
 print(find_min_operation("table", "tbrles", 0, 0))
+
+# Dynamic Programming
+# Top Down
+def convert_string(s1, s2, index1, index2, temp_dict):
+  if index1 == len(s1):
+    return len(s2)-index2
+  if index2 == len(s2):
+    return len(s1)-index1
+  if s1[index1] == s2[index2]:
+    return convert_string(s1, s2, index1+1, index2+1, temp_dict)
+  else:
+    dict_key = str(index1)+str(index2)
+    if dict_key not in temp_dict:
+      delete_op = 1 + convert_string(s1, s2, index1, index2+1, temp_dict)
+      insert_op = 1 + convert_string(s1, s2, index1+1, index2, temp_dict)
+      replace_op = 1 + convert_string(s1, s2, index1+1, index2+1, temp_dict)
+      temp_dict[dict_key] = min(delete_op, insert_op, replace_op)
+    return temp_dict[dict_key]
+
+# Bottom Up
