2cc5c1ba62ee610cc18dc7b1c574aa8214d05f8c
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-09-26 11:51:04 -0400

    Update n_queens.py

--- Python/Code/n_queens.py
+++ Python/Code/n_queens.py
@@ -13,7 +13,48 @@ class NQueens:
       print("\n")
 
   def is_place_safe(self, row_index, col_index):
-    
+    # Check horizontally
+    for i in range(self.n):
+      if self.chess_table[row_index][i] == 1:
+        return False
+    # Top left to right bottom
+    j = col_index
+    for i in range(row_index, -1, -1);
+      if i < 0:
+        break
+      if self.chess_table[i][j] == 1:
+        return False
+      j = j - 1
+
+    # Top right to bottom left
+    j = col_index
+    for i in range(row_index, self.n);
+      if i < 0:
+        break
+      if self.chess_table[i][j] == 1:
+        return False
+      j = j - 1
+
+    return True
+
+  def solve(self, col_index):
+    if col_index == self.n:
+      return True
+
+    for row_index in range(self.n):
+      if self.is_place_safe(row_index, col_index):
+        self.chess_table[row_index][col_index] = 1
+        if self.solve(col_index + 1):
+          return True
+        self.chess_table[row_index][col_index] = 0
+
+    return False
+
+  def solve_NQueens(self):
+    if self.solve(0):
+      self.print_queens()
+    else:
+      print("There is no valid solution")
 
 queens = NQueens(4)
-queens.print_queens()
+queens.solve_NQueens()
