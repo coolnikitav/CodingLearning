b181f2acc676a2581bbe48ea5e1476c2259baba5
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-10-10 14:02:12 -0400

    Update circular_singly_linked_list.py

--- Python/Code/circular_singly_linked_list.py
+++ Python/Code/circular_singly_linked_list.py
@@ -144,9 +144,33 @@ class CSLinkedList:
     self.length -= 1
     return popped_node
 
+  def remove(self, index):
+    if index < 0: or index >= self.length:
+      return None
+    elif index == 0:
+      return self.pop_first()
+    elif index == self.length-1:
+      return self.pop()
+    prev_node = self.get(index-1)
+    popped_node = prev_node.next
+    prev_node.next = popped_node.next
+    popped_node.next = None
+    self.length -= 1
+    return popped_node
+
+  def delete_all(self):
+    if self.length == 0:
+      return
+    self.tail.next = None
+    self.head = None
+    self.tail = None
+    self.length = 0
+
 cslinkedlist = CSLinkedList()
 cslinkedlist.append(10)
 cslinkedlist.append(20)
+cslinkedlist.append(70)
+cslinkedlist.append(80)
 cslinkedlist.prepend(5)
 cslinkedlist.prepend(1)
 cslinkedlist.insert(0, 30)
@@ -165,4 +189,7 @@ print(cslinkedlist.set(2, 11))
 print(cslinkedlist)
 print(cslinkedlist.pop_first())
 print(cslinkedlist.pop())
+print(cslinkedlist)
+print(cslinkedlist.remove(1))
+cslinkedlist.delete(all)
 print(cslinkedlist)
