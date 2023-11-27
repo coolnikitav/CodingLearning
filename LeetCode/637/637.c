cd2bd236cdea82c4d17c9d388f85e74542a652cb
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-11-27 13:24:12 -0500

    Update 637.c

--- LeetCode/637/637.c
+++ LeetCode/637/637.c
@@ -7,26 +7,39 @@ struct TreeNode {
 };
  
 double* averageOfLevels(struct TreeNode* root, int* returnSize){
-    double *result = (double*)calloc(10000,  sizeof(double));
-    struct TreeNode* queue[10010];
+    double* result = (double*)calloc(10000, sizeof(double));
+    struct TreeNode* queue[10000];
     int front = 0, back = 0;
     *returnSize = 0;
-    queue[back++] = root;
-    while(front < back) {
+
+    queue[back] = root;
+    back++;
+
+    while (front < back) {
         double sum = 0.0;
         int len = back - front;
         int cur_back = back;
-        while(front < cur_back) {
-            struct TreeNode* node = queue[front++];
-            if(node->left)
-                queue[back++] = node->left;
-            if(node->right)
-                queue[back++] = node->right;
-            sum += node->val;
+
+        while (front < cur_back) {
+            struct TreeNode* curr_node = queue[front];
+            front++;
+
+            if (curr_node->left) {
+                queue[back] = curr_node->left;
+                back++;
+            }
+
+            if (curr_node->right) {
+                queue[back] = curr_node->right;
+                back++;
+            }
+
+            sum += curr_node->val;
         }
-        result[(*returnSize)++] = sum / len;
-        // *returnSize += 1;
+        result[*returnSize] = sum/len;
+        (*returnSize)++;
     }
+
     return result;
 }
 
