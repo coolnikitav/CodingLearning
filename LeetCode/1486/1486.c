b512d8c0034711a02ac449b9f5594de0dbd402aa
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2023-12-01 09:09:48 -0500

    Update 1486.c

--- LeetCode/1486/1486.c
+++ LeetCode/1486/1486.c
@@ -2,7 +2,7 @@
 
 int xorOperation(int n, int start) {
     int result = start;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n-1; i++) {
         start += 2;
         result ^= start;
     }
