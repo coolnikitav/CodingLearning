968fe337474d7e0a582c0b3c37576c8b8456eed6
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2024-07-05 14:36:50 -0400

    Update Builder.ps1

--- python/Builder.ps1
+++ python/Builder.ps1
@@ -302,3 +302,38 @@ function Build-Selftest {
 }
 
 Build-Selftest
+
+
+
+
+
+
+
+
+
+
+# Path to the zip file in locationB
+$zipFilePath = "C:\Path\To\locationB\YourZipFile.zip"
+
+# Path where the generated script will be saved in locationB
+$generatedScriptPath = "C:\Path\To\locationB\scriptB.ps1"
+
+# Read the zip file and convert it to a Base64 string
+$zipFileBytes = [System.IO.File]::ReadAllBytes($zipFilePath)
+$zipFileBase64 = [System.Convert]::ToBase64String($zipFileBytes)
+
+# Generate scriptB that will embed and install the zip file
+$scriptBContent = @"
+\$zipFileBase64 = '$zipFileBase64'
+\$zipFileBytes = [System.Convert]::FromBase64String(\$zipFileBase64)
+\$zipFilePath = 'C:\Path\To\locationC\YourZipFile.zip'
+
+# Write the zip file to locationC
+[System.IO.File]::WriteAllBytes(\$zipFilePath, \$zipFileBytes)
+Write-Output "Zip file has been installed to \$zipFilePath"
+"@
+
+# Write the generated script to the specified path
+Set-Content -Path $generatedScriptPath -Value $scriptBContent
+
+Write-Output "ScriptB has been generated at $generatedScriptPath"
