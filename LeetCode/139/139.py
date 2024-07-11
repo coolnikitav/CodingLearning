6fa52a9ee7489e01ace4b7aa0c96ad7c9b9e52d0
Nikita Varfolomeev <30304422+coolnikitav@users.noreply.github.com>
2024-07-11 14:48:39 -0400

    Update 139.py

--- LeetCode/139/139.py
+++ LeetCode/139/139.py
@@ -5,26 +5,52 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        # traverse the word. On every letter we can add it to the current word or start new word
-        dp = [False] * len(s)
+        # Recursion
+        '''
+        def wordBreakHelper(i):
+            if i == len(s):
+                return True
+
+
+            for key in wordDict:
+                word = s[i:i+len(key)]
+                if word == key and wordBreakRecursive(i+len(key)):
+                    return True
+            
+            return False
+
+        return wordBreakRecursive(0)
+        '''
+
+        # Recursion with memo
+        '''
+        memo = {}
+        def wordBreakHelper(i):
+            if i == len(s):
+                return True
+
+            if i in memo:
+                return memo[i]
+
+            for key in wordDict:
+                word = s[i:i+len(key)]
+                if word == key and wordBreakHelper(i+len(key)):
+                    memo[i] = True
+                    return True
+            
+            memo[i] = False
+            return False
+
+        return wordBreakHelper(0)
+        '''
+
+        # DP
+        dp = [False] * (len(s)+1)
+        dp[-1] = True
 
         for i in range(len(s)-1,-1,-1):
             for key in wordDict:
-                if i+len(key) >= len(s):
-                    word = s[i:len(s)]
-                    if word == key:
-                        dp[i] = True
-                else:
-                    word = s[i:i+len(key)]
-                    if word == key and dp[i+len(key)]:
-                        dp[i] = True
+                word = s[i:min(i+len(key), len(s))]
+                if word == key and dp[min(i+len(key), len(s))]:
+                    dp[i] = True
         return dp[0]
-
-test = Solution()
-print(test.wordBreak("leetcode", ["leet","code"]))
-print(test.wordBreak("applepenapple", ["apple","pen"]))
-print(test.wordBreak("catsandog", ["cats","dog","sand","and","cat"]))
-print(test.wordBreak("aaaaaaa", ["aaaa","aaa"]))
-print(test.wordBreak("cars", ["car","ca","rs"]))
-print(test.wordBreak("cars", ["ca","rs","car"]))
-print(test.wordBreak("ab", ["a","b"]))
